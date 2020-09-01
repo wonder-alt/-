e8ccebdaa5783ffe56efc21bbedc3a131bfc56b4
jack deng <1181167805@qq.om>
2020-09-01 15:05:42 +0800

    test

---
+++ test---8-30/test---8-30/test-1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	char num = 'C';
+		printf("%c\n", num);
+	return 0;
+}
---
+++ test---8-30/test---8-30/test2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()[注：此处应知识问题无法进行续接，若忘记则将注解删除后进行生成即可发现问题]
+{
+	int age = 12;
+	printf("%d\n", age);
+	return 0;
+}
+
+
