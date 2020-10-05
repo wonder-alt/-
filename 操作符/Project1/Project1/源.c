8d467783727cc27672b4636ebfcbbef6d9233bfa
jack deng <1181167805@qq.om>
2020-10-05 21:47:25 +0800

    test

---
+++ 操作符/Project1/Project1/源.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//int main()
+//{
+//	double a = 5 / 2.0;
+//	printf("%lf\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = a >> 1;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 5;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	c = a&b;
+//	d = a | b;
+//	e = a^b;//两个相同为一，其余为二
+//	return 0;
+//}
+
+//相互交换
+//int main()
+//{
+//	int a = 5;
+//	int b = 10;
+//////加减法
+////	a = a + b;
+////		b = a - b;
+////		a = a - b;
+//	
+//	////异或的方法
+//	//a = a^b;
+//	//b = a^b;
+//	//a = a^b;
+//		printf("a=%d b=%d", a, b);
+//		return 0;
+//}
+
+
+////求一个整数存储在内存中二进制的个数
+//int main()
+//{
+//	int count = 0;
+//	int num = 0;
+//	scanf("%d", &num);
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num >> i) & 1))
+//		{
+//			count++;
+//		}
+//		printf("%d\n", count);
+//	}
+//
+//	return 0;
+//}
