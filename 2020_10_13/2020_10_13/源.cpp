1fb56eca77bfbd003b69074bca08ee4f1b7ef72d
jack deng <1181167805@qq.om>
2020-10-13 21:22:02 +0800

    test

---
+++ 2020_10_13/2020_10_13/源.cpp
@@ -0,0 +1,61 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c,min;
+//	cin >> a >> b >> c;
+//		if (a > b)
+//	{
+//		min = b;
+//	}
+//	else
+//	{
+//		min = a;
+//	}
+//	if (c < min)
+//	{
+//		min = c;
+//	}
+//	cout << min << endl;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	double x, y,a,s;
+//	cin >> x >> y;
+//	s = 3.14 * 4 * 4 - 3.14 * 2 * 2;
+//	a = x*x + y*y;
+//	if (a >= 4 && a <= 16)
+//	{
+//		
+//		cout << s<<endl;
+//	}
+//	else
+//	{
+//		cout <<"0"<< endl;
+//	}
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	char a;
+//	while (1)
+//	{
+//		a = getchar();
+//		if (a >= 'a'&&a <= 'z' || a >= 'A'&&a <= 'Z')
+//		{
+//			a = a + 3;
+//			if (a > 'z'&&a <= 'z' + 3 || a > 'Z'&&a <= 'Z' + 3)
+//			{
+//				a = a - 26;
+//			}
+//		}
+//		cout << a << endl;
+//	}
+//	return 0;
+//}
