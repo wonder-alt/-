01540b76660c2a4f634a641a028bc6d3d3467dc9
jack deng <1181167805@qq.om>
2020-10-13 17:25:58 +0800

    test

---
+++ 2020_10_12/2020_10_12/源.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+// char a;
+// a=getchar();
+// cout<< a<<endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	char str[] = "hello";
+//	gets(str);//把数字赋给给str
+//	puts(str);//输出str
+//	cout << str << endl；
+//	return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	char str[] = "ehllo";
+//	scanf("%s",str);//%s是字符串
+//	cout << str << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, x=1, y=1;
+//	a = x--&&y--;
+//	cout << a << endl;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int x = 1.78;
+//	x = x + 3;
+//	cout << x << endl;
+//	return 0;
+//}
