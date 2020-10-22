0df76f4ceac6f81364550295d5ad5c365dfd229b
jack deng <1181167805@qq.om>
2020-10-22 22:23:20 +0800

    test

---
+++ 2020-10-22/2020-10-22/源.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int i,  sum;
+//	i = 0;
+//	sum = 0;
+//	for (i = 1; i <=5; i += 2)
+//	{
+//		sum = sum + i;
+//	}
+//	cout << sum << endl;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int i ,n;
+//	int x = 1;
+//    cin  >> n>>i;
+//	if (i <= 0)
+//	{
+//		cout << "ÊäÈë´íÎó" << endl;
+//	}
+//	else
+//	{
+//		for (i = 1; i <= n; i++)
+//		{
+//			x *= i;
+//		}
+//	}
+//	cout << x << endl;
+//	return 0;
+//}
