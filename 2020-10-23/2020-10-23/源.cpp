034378819560f79e477da6d899455f8c2397825d
jack deng <1181167805@qq.om>
2020-10-24 12:17:30 +0800

    1

---
+++ 2020-10-23/2020-10-23/源.cpp
@@ -0,0 +1,39 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n,i;
+//	cin >> n;
+//	 double sum = 0;
+//	for (i = 1; i <=n; i++)
+//	{
+//		sum = sum+1.0 / i;
+//	}
+//	cout << sum << endl;
+//	return 0;
+//}
+//
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int i;
+//	cin >> i;
+//	while (i)
+//	{
+//		cout << i % 10 << endl;
+//		i = i / 10;
+//	}
+//	return 0;
+//}
+
+
+#include<iostream>
+int main()
+{
+	int	unsigned  num;
+	cin
+	return 0;
+}
